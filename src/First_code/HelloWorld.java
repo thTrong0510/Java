0c0aa3ef68459964a3b75d6e9bbb083c9b1b8c58
thTrong0510 <vthanhtrongng@gmail.com>
2024-11-29 20:12:21 +0700

    doi ten lab01 thanh loop

--- src/First_code/HelloWorld.java
+++ src/First_code/HelloWorld.java
@@ -9,8 +9,13 @@ public class HelloWorld {
 		//format %d số nguyên, %f số thực mặc định là 6 số sau dấu ',' %.3f 3 số, %s chuỗi
 		System.out.printf("Toi %d tuoi", 20);
 		
+		//Scanner 
 		Scanner scanner = new Scanner(System.in);
 		System.out.printf("Nhan %s cua ban: ", "ten");
 		String name = scanner.nextLine();
+		scanner.close();
+		
+		//hàm toán học object Math
+		System.out.println(Math.max(10, 5));
 	}
 }
