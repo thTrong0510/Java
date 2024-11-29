2abbb6ef2a8c4b9fd1b292b6c8359933dab37c7e
thTrong0510 <vthanhtrongng@gmail.com>
2024-11-30 01:11:40 +0700

    them phan access modifier

--- src/OOP/OOP.java
+++ src/OOP/OOP.java
@@ -1,6 +1,17 @@
 package OOP;
 
 public class OOP {
+	
+	//Tạo Object của 1 class trong cùng package hoặc được import vào 
+	/*
+	  Nếu thuộc tính: 
+	  Public: có thể dùng object truy cập vào
+	  protected: cùng package thì như public 
+	  			 khác package nếu được kế thừa như public - ko được kế thừa như private
+	  default: cùng package như public - khác package như priavte
+	  private: không thể dùng object truy cập vào được 
+	*/
+	
 	public static void main(String[] args) {
 		SinhVien Trong = new SinhVien("Thanh Trong", 20);
 		Trong.introduce("Trung Binh");
